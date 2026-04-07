53a000a1d4f950780f356ed4d7123f908e4da64b
newbie656 <tmdgnxmfhf@gmail.com>
2026-04-07 23:09:57 +0900

    임포트 하면서 빠진 것들 보완

---
+++ Assets/AI_test/Scripts/Enemy/EnemyAnimationRelay.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class EnemyAnimationRelay : MonoBehaviour
+{
+    private EnemyStateManager parentEnemy;
+
+    void Awake()
+    {
+        // 내 부모님(EnemyStateManager)을 찾아서 기억해둠
+        parentEnemy = GetComponentInParent<EnemyStateManager>();
+    }
+
+    // 애니메이션 이벤트에서 이 함수를 부를 겁니다.
+    public void PerformAttack()
+    {
+        if (parentEnemy != null) parentEnemy.PerformAttack();
+    }
+
+    // 공격 애니메이션이 끝날 때 부를 함수
+    public void OnAttackAnimationFinished()
+    {
+        if (parentEnemy != null) parentEnemy.OnAttackAnimationFinished();
+    }
+}
