753b653564a3fe6da624a39c543bc1de81fe5c5b
newbie656 <tmdgnxmfhf@gmail.com>
2026-05-05 23:50:49 +0900

    텍스트 창 추가, 폰트(깨진상태) 추가
    텍스트 넘기기 기본키: 역슬레시(\)

--- Assets/AI_test/Scripts/Boss/Stage1_Golem/BossStates/BossDeadState.cs
+++ Assets/AI_test/Scripts/Boss/Stage1_Golem/BossStates/BossDeadState.cs
@@ -6,12 +6,16 @@ public class BossDeadState : IBossState
     {
         Debug.Log("보스 사망!");
 
-        // 1. 이동 및 기능 정지
         boss.navMeshAgent.isStopped = true;
         boss.navMeshAgent.enabled = false; // 땅 고정 해제 (가라앉는 애니메이션을 위해)
         boss.GetComponent<Collider>().enabled = false;
 
-        if (boss.animator != null) boss.animator.SetTrigger("dead");
+        //Has Exit Time 이슈로 애니메이션 전환 대신 강제로 재생하도록 변경
+        //if (boss.animator != null) boss.animator.SetTrigger("dead");
+        if (boss.animator != null)
+        {
+            boss.animator.Play("Dead");
+        }
 
         if (boss.deadSound != null || boss.deadSound2 != null)
         {
@@ -19,13 +23,10 @@ public class BossDeadState : IBossState
             SoundManager.Instance.PlaySFX3D(boss.deadSound2, boss.transform.position);
         }
 
-        // 3. 약점 숨기기 (혹시 켜져있다면)
         if (boss.weakPointObject != null) boss.weakPointObject.SetActive(false);
 
-        // 4. 죽음 시퀀스 시작
         boss.StartDeathSequence();
 
-        // 5. 스크립트 비활성화
         boss.enabled = false;
     }
 
---
+++ Assets/Message/Scripts/MessageManager.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using TMPro;
+using System.Collections.Generic;
+
+public class MessageManager : MonoBehaviour
+{
+    public static MessageManager instance;
+
+    [Header("UI Elements")]
+    public GameObject messagePanel;
+    public TextMeshProUGUI messageText;
+    public TextMeshProUGUI TextKeyGuide;
+
+    [Header("Input Settings")]
+    public KeyCode nextMessageKey = KeyCode.Backslash;
+
+    private Queue<string> messageQueue = new Queue<string>();
+    private bool isMessageActive = false;
+
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+        messagePanel.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (isMessageActive && Input.GetKeyDown(nextMessageKey))
+        {
+            DisplayNextMessage();
+        }
+    }
+
+    public void StartMessage(string[] lines)
+    {
+        messageQueue.Clear();
+
+        foreach (string line in lines)
+        {
+            messageQueue.Enqueue(line);
+        }
+
+        TextKeyGuide.text = $"Press: '{nextMessageKey.ToString()}'";
+
+        messagePanel.SetActive(true);
+        isMessageActive = true;
+        DisplayNextMessage();
+    }
+
+    public void DisplayNextMessage()
+    {
+        if (messageQueue.Count == 0)
+        {
+            EndMessage();
+            return;
+        }
+
+        string nextMessage = messageQueue.Dequeue();
+        messageText.text = nextMessage;
+    }
+
+    private void EndMessage()
+    {
+        messagePanel.SetActive(false);
+        isMessageActive = false;
+    }
+}
